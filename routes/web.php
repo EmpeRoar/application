a9080c003d0a2b073e7c29275ce2fa72d3aac240
JuliusBacosa <juliusbacosa@gmail.com>
2016-08-27 20:46:16 +0800

    Route and Model implementation

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,39 @@
 Route::get('/', function () {
     echo "Laravel Hello World";
 });
+
+// create an item 
+Route::post('test', function () {
+    echo "POSTED";
+});
+
+// read an item
+Route::get('test', function () {
+    echo '<form action="test" method="POST">';
+    echo '<input type="submit" value="submit">';
+    echo '<input type="hidden" name="_token" value="'.csrf_token().'">';
+    echo '<input type="hidden" name="_method" value="PUT">';
+    echo '</form>';
+});
+
+// update item 
+Route::put('test', function () {
+    echo "PUT";
+});
+
+//delete item
+Route::delete('test', function () {
+    echo 'DELETE';
+});
+
+Route::get('test123','TestController@index');
+
+Route::get('customer/{id}',function($id){
+    $customer = App\Customer::find($id);
+    echo 'Hello my name is '.$customer->name; 
+});
+
+Route::get('get_customer',function(){
+    $customer = App\Customer::where('name','=','bob')->first();
+    echo $customer->name;
+});
